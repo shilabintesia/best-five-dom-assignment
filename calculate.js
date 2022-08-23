1f3eee9640ed96f37d97a490c7046299b5309425
shilabintesia <shilaislamsia@gmail.com>
2022-08-23 21:27:14 +0600

    first commit

---
+++ calculate.js
@@ -0,0 +1,44 @@
+document.getElementById('btn-calculate').addEventListener('click', function() {
+    const perPlayerField = document.getElementById('per-player');
+    const perPlayerAmountString = perPlayerField.value;
+    const perPlayerAmount = parseFloat(perPlayerAmountString);
+
+    const playerExpencesField = document.getElementById('player-expences');
+    const totalPlayerAmount = perPlayerAmount * playerCount;
+    if (isNaN(totalPlayerAmount)) {
+        alert('please enter a valid input')
+    } else {
+        playerExpencesField.innerText = totalPlayerAmount;
+    }
+})
+
+document.getElementById('calculate-total').addEventListener('click', function() {
+    const managerField = document.getElementById('manager');
+    const managerAmountString = managerField.value;
+    const managerAmount = parseFloat(managerAmountString);
+
+
+    const coachField = document.getElementById('coach');
+    const coachAmountString = coachField.value;
+    const coachAmount = parseFloat(coachAmountString);
+
+    const totalAmountField = document.getElementById('total-amount');
+    const totalAmountString = totalAmountField.innerText;
+    const totalAmount = parseFloat(totalAmountString);
+
+    const perPlayerField = document.getElementById('per-player');
+
+    const perPlayerAmountString = perPlayerField.value;
+    const perPlayerAmount = parseFloat(perPlayerAmountString);
+    const totalPlayerAmount = perPlayerAmount * playerCount;
+
+    const allTotal = totalPlayerAmount + coachAmount + managerAmount;
+
+
+    if (isNaN(allTotal)) {
+        alert('please enter a valid input')
+    } else {
+
+        totalAmountField.innerText = allTotal;
+    }
+})
---
+++ selectFive.js
@@ -0,0 +1,39 @@
+const selectedPlayer = [];
+
+let playerCount = 0;
+
+function displayPlayerName(player) {
+    const selectedPlayerName = document.getElementById('selected-player')
+    selectedPlayerName.innerHTML = '';
+    for (let i = 0; i < player.length; i++) {
+
+        if (i >= 5) {
+            alert('you can not add player anymore')
+            return;
+
+        } else {
+            const playerName = selectedPlayer[i]
+            const tr = document.createElement('tr')
+            tr.innerHTML = `
+            <td>${i + 1}</td>
+            <td> ${playerName}</td>
+            `
+            selectedPlayerName.appendChild(tr)
+        }
+    }
+    playerCount++
+}
+
+function addToSelectArea(Element) {
+    const addedPlayer = Element.parentNode.children[0].innerText
+    selectedPlayer.push(addedPlayer);
+
+    if (selectedPlayer.length >= 6) {
+        alert('you can not add player anymore')
+        return;
+
+    } else {
+        document.getElementById('selected-number').innerText = selectedPlayer.length;
+    }
+    displayPlayerName(selectedPlayer)
+}
